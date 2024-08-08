950141c2a5546f8a29bb206997a925358ad6023f
GCespedes <giovannice22@hotmail.com>
2024-08-08 01:09:35 -0500

    actualizado

--- FRONT/src/app/views/about/about.component.ts
+++ FRONT/src/app/views/about/about.component.ts
@@ -8,45 +8,38 @@ import { Component } from '@angular/core';
 export class AboutComponent {
   list1 = [
     {
-      img: 'assets/contact.svg',
+      img: '',
       name: 'Promover la salud y el bienestar',
       description:
         'En La Frutita, nuestra misión es ofrecer opciones gastronómicas que nutran el cuerpo y deleiten el paladar, utilizando ingredientes frescos y locales para fomentar un estilo de vida saludable.',
     },
     {
-      img: 'assets/team.svg',
+      img: '',
       name: 'Compromiso con la frescura y la calidad',
       description:
         'Valoramos la frescura de nuestros ingredientes por encima de todo. Nos esforzamos por seleccionar los productos más frescos y de alta calidad.',
     },
     {
-      img: 'assets/door.svg',
+      img: '',
       name: 'Excelencia en el servicio al cliente.',
       description:
         'En La Frutita, nos esforzamos por brindar una experiencia acogedora y personalizada a cada cliente, asegurando que se sientan bienvenidos y satisfechos en cada visita.',
     },
     {
-      img: 'assets/menu.svg',
+      img: '',
       name: 'Sostenibilidad y la responsabilidad',
       description:
         'Nos comprometemos a operar de manera sostenible, minimizando nuestro impacto ambiental mediante el uso de ingredientes orgánicos y prácticas de cocina que respeten el medio ambiente.',
     },
   ];
 
-  list2 = [
-    'assets/contact.svg',
-    'assets/contact.svg',
-    'assets/contact.svg',
-    'assets/contact.svg',
-    'assets/contact.svg',
-    'assets/contact.svg',
-  ];
+  list2 = ['', '', '', '', '', ''];
 
   slider: any;
   defaultTransform: any;
 
   goNext() {
-    this.defaultTransform = this.defaultTransform - 600;
+    this.defaultTransform = this.defaultTransform - 400;
     if (Math.abs(this.defaultTransform) >= this.slider.scrollWidth / 1.7)
       this.defaultTransform = 0;
     this.slider.style.transform = 'translateX(' + this.defaultTransform + 'px)';
@@ -54,7 +47,7 @@ export class AboutComponent {
 
   goPrev() {
     if (Math.abs(this.defaultTransform) === 0) this.defaultTransform = 0;
-    else this.defaultTransform = this.defaultTransform + 398;
+    else this.defaultTransform = this.defaultTransform + 400;
     this.slider.style.transform = 'translateX(' + this.defaultTransform + 'px)';
   }
 
--- FRONT/src/app/views/team/team.component.ts
+++ FRONT/src/app/views/team/team.component.ts
@@ -3,8 +3,99 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.css'],
 })
 export class TeamComponent {
+  selected = '';
 
+  list1 = [
+    {
+      Name: 'Cocineros',
+      Content: [
+        {
+          img: 'img',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+        {
+          img: 'img',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+        {
+          img: 'img',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+        {
+          img: 'img',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+      ],
+    },
+    {
+      Name: 'Administracion',
+      Content: [
+        {
+          img: 'img2',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+        {
+          img: 'img2',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+        {
+          img: 'img2',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+      ],
+    },
+    {
+      Name: 'Atencion',
+      Content: [
+        {
+          img: 'img3',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+        {
+          img: 'img3',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+        {
+          img: 'img3',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+      ],
+    },
+    {
+      Name: 'Barman',
+      Content: [
+        {
+          img: 'img',
+          title: 'Nombre y Apellido',
+          description: 'Descripcion del colaborador',
+        },
+      ],
+    },
+  ];
+
+  content = this.list1[0].Content;
+
+  newselectect(name: string) {
+    this.selected = name;
+    const selected = this.list1.find((item) => item.Name === name);
+
+    this.content = selected?.Content || [];
+  }
+
+  ngOnInit(): void {
+    this.newselectect('Cocineros');
+  }
 }
